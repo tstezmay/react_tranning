a64dd0b1c0abf6393ce63df7b53745f532b80c0a
Nathaphol Phimpha <nuttapon_pim@truecorp.co.th>
2019-08-17 14:10:28 +0700

    test

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 const User = require('./models/user');
-const Login = require('./service/authen');
-const calculator = require('./service/calculator');
+const Login = require('./services/authen');
+const calculator = require('./services/calculator');
 const DatabeseConnect = require('./databeseConnect');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ name: 'bas' });
+});
+app.get('/users', (req, res) => {
+  res.json([{ name: 'bas' },{name: 'bas2'}]);
+});
+
+app.listen(3000, () => { console.log('running at 3000') });
 
 // let username = 'bas';
 // console.log(username);
 // console.log(User);
 // Login('admin','password');
-console.log(calculator.plus(2,4));
-console.log(calculator.subtract(5,4));
+// console.log(calculator.plus(2,4));
+// console.log(calculator.subtract(5,4));
 
-let resultPlus = require('./service/calculator').plus(40,3);
-console.log(resultPlus);
+// let resultPlus = require('./services/calculator').plus(40,3);
+// console.log(resultPlus);
 
-let addResult = calculator.addOne(44);
-console.log(addResult);
-let db = new DatabeseConnect();
-db.connect();
-db.disconnect();
+// let addResult = calculator.addOne(44);
+// console.log(addResult);
+// let db = new DatabeseConnect();
+// db.connect();
+// db.disconnect();
